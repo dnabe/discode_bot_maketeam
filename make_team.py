bf29d6b81942fdd538ade9d95bffa560b5021249
鍋谷 太希 <b1849701@planet.kanazawa-it.ac.jp>
2018-10-20 00:19:52 +0900

    チーム分け

--- make_team.py
+++ make_team.py
@@ -11,12 +11,37 @@ async def on_ready():
 # メッセージによる処理
 @client.event
 async def on_message(message):
-    if message.content.startswith('/role'):
-        user = [member.display_name for member in client.get_all_members()]
+    
+    cmd_1 = command(message.content,0)
+    red_team = command(message.content,1)
+    blue_team = command(message.content,2)
+    l = len(user)
+    num_team = list_team()
+    list_red = []
+    list_blue = []
+    
+    if message.content.startswith('/que'):
+        user = [member.display_name for member in client.get_all_members() if member.voice.voice_channel is not None]
         q = user[random.randrange(0,len(user))]
         reply = '出題者は' + str(q)
             
         await client.send_message(message.channel, reply)
+    
+    elif cmd_1 == '/team'
+        user = [member.display_name for member in client.get_all_members() if member.voice.voice_channel is not None]
+        for i in range(int(red_team)):
+            list_red[i] = user[random.randrange(0,len(user))]
+            user.remove(list_red[i])
+        list_blue = user
+        reply = red_team + 'vs' + blue_team
+            
+        await client.send_message(message.channel,reply)
+        
+def command(message,n):
+    try:
+        return message.split(' ')[n]
+    except IndexError:
+        return ''
     
 
 # botの接続と起動
