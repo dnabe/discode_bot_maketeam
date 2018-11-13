a47797b79fb4266e3725751ded0065b385788b60
鍋谷 太希 <b1849701@planet.kanazawa-it.ac.jp>
2018-11-13 23:24:51 +0900

    メンション追加

--- reaction_bot.py
+++ reaction_bot.py
@@ -10,6 +10,7 @@ async def on_ready():
 async def on_message(message):
 
     if message.author.id == '433511130439090178':
-        await client.send_message(message.channel,'hello')
+        reply = f'{client.get_all_members.mention} たくみんが話したよ！'
+        await client.send_message(message.channel,reply)
 
 client.run('NTExODk2MTE5NTY3OTc0NDAw.Dsxl7g.aFBgRs3aVBpCphX_czE30dQCOAw')
