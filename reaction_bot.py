96a001aed728ba6c0b50d41848d4df4052824898
鍋谷 太希 <b1849701@planet.kanazawa-it.ac.jp>
2018-11-14 01:41:43 +0900

    修正

--- reaction_bot.py
+++ reaction_bot.py
@@ -16,7 +16,7 @@ async def on_message(message):
         #reply = '<@&511936816677519382> たくみんが話したよ！' #くらくらサーバ
         #reply = '<@324831068412706816> <@268972787601899539> <@379326250717544450> <@381641619239338005> <@336050839556849664> <@335385731071475714> <@322690513507581952> <@287753691270742016> <@448872826187808768> たくみんが話したよ！'
         await client.send_message(message.channel,reply)
-        
-        await client.delete_message(reply)
+
+        await client.delete_message(message.channel,reply)
 
 client.run('NTExODk2MTE5NTY3OTc0NDAw.Dsxl7g.aFBgRs3aVBpCphX_czE30dQCOAw')
