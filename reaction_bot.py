8f03a662491f18953169e29e7e1712f84c249b84
鍋谷 太希 <b1849701@planet.kanazawa-it.ac.jp>
2018-11-14 01:02:57 +0900

    いったんidで

--- reaction_bot.py
+++ reaction_bot.py
@@ -10,7 +10,7 @@ async def on_ready():
 async def on_message(message):
 
     if message.author.id == '279562735635660800':
-        reply = '@劇的ファン たくみんが話したよ！'
+        reply = '<@324831068412706816> <@268972787601899539> <@379326250717544450> <@381641619239338005> <@336050839556849664> <@335385731071475714> <@322690513507581952> <@287753691270742016> <@448872826187808768> たくみんが話したよ！'
         await client.send_message(message.channel,reply)
 
 client.run('NTExODk2MTE5NTY3OTc0NDAw.Dsxl7g.aFBgRs3aVBpCphX_czE30dQCOAw')
