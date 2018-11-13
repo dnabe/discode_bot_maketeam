3a9342e513617a950755178d0848e35e37c10048
鍋谷 太希 <b1849701@planet.kanazawa-it.ac.jp>
2018-11-14 01:32:35 +0900

    自分でメッセージ消す

--- reaction_bot.py
+++ reaction_bot.py
@@ -1,4 +1,5 @@
 import discord
+import time
 
 client = discord.Client()
 
@@ -9,10 +10,13 @@ async def on_ready():
 @client.event
 async def on_message(message):
 
-    if message.author.id == '279562735635660800': #たくみんのid
-    #if message.author.id == '433511130439090178': #鍋谷のid
-        reply = '<@&511936816677519382> たくみんが話したよ！'
+    #if message.author.id == '279562735635660800': #たくみんのid
+    if message.author.id == '433511130439090178': #鍋谷のid
+        reply = '<@&458110776964808706> たくみんが話したよ！' #テスト用
+        #reply = '<@&511936816677519382> たくみんが話したよ！' #くらくらサーバ
         #reply = '<@324831068412706816> <@268972787601899539> <@379326250717544450> <@381641619239338005> <@336050839556849664> <@335385731071475714> <@322690513507581952> <@287753691270742016> <@448872826187808768> たくみんが話したよ！'
         await client.send_message(message.channel,reply)
+        time.sleep(5.0)
+        await client.delete_message(reply)
 
 client.run('NTExODk2MTE5NTY3OTc0NDAw.Dsxl7g.aFBgRs3aVBpCphX_czE30dQCOAw')
