c1c97e93b669c01b96053e2ce3f2d44855508a18
鍋谷 太希 <b1849701@planet.kanazawa-it.ac.jp>
2018-11-13 23:30:13 +0900

    いったん

--- reaction_bot.py
+++ reaction_bot.py
@@ -10,7 +10,7 @@ async def on_ready():
 async def on_message(message):
 
     if message.author.id == '433511130439090178':
-        reply = f'{client.get_all_members.mention} たくみんが話したよ！'
+        reply = '@everyone たくみんが話したよ！'
         await client.send_message(message.channel,reply)
 
 client.run('NTExODk2MTE5NTY3OTc0NDAw.Dsxl7g.aFBgRs3aVBpCphX_czE30dQCOAw')
