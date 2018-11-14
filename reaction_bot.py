13048253eca1509bb5da23ce4717f4e7baff825a
鍋谷 太希 <b1849701@planet.kanazawa-it.ac.jp>
2018-11-14 16:56:15 +0900

    いったんもどす

--- reaction_bot.py
+++ reaction_bot.py
@@ -11,14 +11,14 @@ async def on_ready():
 @client.event
 async def on_message(message):
 
-    if int(datetime.datetime.now().strftime('%H')) >= 13 and int(datetime.datetime.now().strftime('%H')) <= 17:
-        #if message.author.id == '279562735635660800': #たくみんのid
-        if message.author.id == '433511130439090178': #鍋谷のid
-            reply = '<@&458110776964808706> テスト' #テスト用
-            #reply = '<@&511936816677519382> たくみんが話したよ！' #くらくらサーバ
-            #reply = '<@324831068412706816> <@268972787601899539> <@379326250717544450> <@381641619239338005> <@336050839556849664> <@335385731071475714> <@322690513507581952> <@287753691270742016> <@448872826187808768> たくみんが話したよ！'
-            msg = await client.send_message(message.channel,reply)
-            time.sleep(5.0)
-            await client.delete_message(msg)
+
+    if message.author.id == '279562735635660800': #たくみんのid
+    #if message.author.id == '433511130439090178': #鍋谷のid
+        #reply = '<@&458110776964808706> テスト' #テスト用
+        reply = '<@&511936816677519382> たくみんが話したよ！' #くらくらサーバ
+        #reply = '<@324831068412706816> <@268972787601899539> <@379326250717544450> <@381641619239338005> <@336050839556849664> <@335385731071475714> <@322690513507581952> <@287753691270742016> <@448872826187808768> たくみんが話したよ！'
+        msg = await client.send_message(message.channel,reply)
+        time.sleep(5.0)
+        await client.delete_message(msg)
 
 client.run('NTExODk2MTE5NTY3OTc0NDAw.Dsxl7g.aFBgRs3aVBpCphX_czE30dQCOAw')
