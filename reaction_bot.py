23dfdc15ad07062e4e40a3f6b1300a6a45a57d8b
鍋谷 太希 <b1849701@planet.kanazawa-it.ac.jp>
2018-11-14 17:34:40 +0900

    またもどす

--- reaction_bot.py
+++ reaction_bot.py
@@ -12,17 +12,17 @@ async def on_ready():
 async def on_message(message):
 
 
-    #if message.author.id == '279562735635660800': #たくみんのid
-        #reply = '<@&511936816677519382> たくみんが話したよ！' #くらくらサーバ
+    if message.author.id == '279562735635660800': #たくみんのid
+        reply = '<@&511936816677519382> たくみんが話したよ！' #くらくらサーバ
 
-    if message.author.id == '433511130439090178': #鍋谷のid
+    #if message.author.id == '433511130439090178': #鍋谷のid
         #reply = '<@&458110776964808706> テスト' #テスト用
-        reply = 'test' #test
+        #reply = 'test' #test
 
-        if int(datetime.datetime.now().strftime('%H')) >= 17:
+        #if int(datetime.datetime.now().strftime('%H')) >= 17:
         #if int(datetime.datetime.now().strftime('%H')) >= 13 and int(datetime.datetime.now().strftime('%H')) <= 16:
-            msg = await client.send_message(message.channel,reply)
-            time.sleep(5.0)
-            await client.delete_message(msg)
+        msg = await client.send_message(message.channel,reply)
+        time.sleep(5.0)
+        await client.delete_message(msg)
 
 client.run('NTExODk2MTE5NTY3OTc0NDAw.Dsxl7g.aFBgRs3aVBpCphX_czE30dQCOAw')
